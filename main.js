b076ce8617bf4018837bf2be9f9255d17205b270
MakerOfDopamine <Harry_wct@outlook.com>
2026-03-30 18:20:54 +1100

    First commit
    Finally got around to starting this

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let player = {
-    is_climbing: false,
     height: new Decimal(0),
     level: new Decimal(1)
 }
+let t = Date.now();
+let dt = Date.now();
 
 function setText(id, condition, truetext="", falsetext="") {
     if (typeof condition === "boolean") {
@@ -16,8 +17,8 @@ function setStyle(id, styleid, stylevalue) {
     document.getElementById(id).style[styleid] = stylevalue;
 }
 
-function getHeightPerTick() {
-    let speed = new Decimal(1).div(20)
+function getHeightPerSec() {
+    let speed = new Decimal(1)
     if (1 == 0) {
         speed = speed.mul(1.5)
     }
@@ -28,24 +29,35 @@ function getTowerFloorHeight(floor) {
     return new Decimal(10).mul(new Decimal(floor).sub(1).pow_base(1.01))
 }
 
-function start_climbing() {
-    player.is_climbing = true
-
+function getUnlocks() {
+    if (player.level.lt(10)) {
+        return "Climb to floor 10 to unlock something new."
+    } else {
+        return "You have unlocked everything so far!"
+    }
 }
 
 if (load()) {alert("Loaded save.")}
 
 let floor_height = getTowerFloorHeight(player.level)
-setInterval(() => {
-    setText("tower-status", player.is_climbing, "You are climbing the Tower of Ascension.", "You are at the foot of an infinitely tall tower.")
-    setText("tower-height", player.is_climbing, `You are currently at ${format(player.height)} m / ${format(floor_height)} m.`)
-    setText("tower-level", player.is_climbing, `You are currently on floor ${format(player.level, player.level.gte(1e9) ? 2 : 0)}.`)
-    if (player.is_climbing) {
-        player.height = player.height.add(getHeightPerTick())
-        if (player.height.gte(floor_height)) {
-            player.height = player.height.sub(floor_height)
-            player.level = player.level.add(1)
-            floor_height = getTowerFloorHeight(player.level)
-        }
+let mainLoop = setInterval(() => {
+    dt = (Date.now() - t) / 1000
+    setText("tower-status", "You are climbing the Tower of Ascension.")
+    setText("tower-height", `You are currently at ${format(player.height)} m / ${format(floor_height)} m.`)
+    setText("tower-level" , `You are currently on floor ${format(player.level, player.level.gte(1e9) ? 2 : 0)}.`)
+    player.height = player.height.add(getHeightPerSec().mul(dt))
+    if (player.height.gte(floor_height)) {
+        let levels = Decimal.affordGeometricSeries(player.height, 10, 1.01, player.level)
+        let totalDist = Decimal.sumGeometricSeries(levels, 10, 1.01, player.level)
+        player.height = player.height.sub(totalDist)
+        player.level = player.level.add(levels)
+        floor_height = getTowerFloorHeight(player.level)
     }
+    setText("next-unlock", getUnlocks())
+
+    t = Date.now()
 }, 50)
+
+let buttonLoop = setInterval(() => {
+    manageTabs()
+}, 250)
---
+++ tabs.js
@@ -0,0 +1,14 @@
+const tabList = ["tower", "xp"]
+
+function switchTab(tabn) {
+    for (let i of tabList) {
+        document.getElementById(i).style.display = "none";
+    }
+    document.getElementById(tabList[tabn]).style.display = "inline-block";
+}
+
+function manageTabs() {
+    for (let i of tabList) {
+        document.getElementById(i + "-tab-button").style.display = 1;
+    }
+}
